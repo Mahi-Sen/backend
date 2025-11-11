beb8f6b8bdba20aa7365c58bbbed53f5e476c445
Mahi-Sen <155892772+Mahi-Sen@users.noreply.github.com>
2025-11-11 13:46:03 +0530

    Update database.py

--- core/database.py
+++ core/database.py
@@ -1,20 +1,14 @@
+# core/database.py (FINAL, CORRECTED VERSION)
 from motor.motor_asyncio import AsyncIOMotorClient
 from .config import MONGO_URI, DATABASE_NAME
+from bson import ObjectId # Import this
 
 client = AsyncIOMotorClient(MONGO_URI)
 database = client[DATABASE_NAME]
 user_collection = database.get_collection("users")
 
+# This helper is now simpler or you can remove it if you always use the model
 def user_helper(user) -> dict:
-    return {
-        "id": str(user["_id"]),
-        "username": user["username"],
-        "access_key": user["access_key"],
-        "is_active": user["is_active"],
-        "api_calls_total": user["api_calls_total"],
-        "api_call_limit": user.get("api_call_limit"),
-        "created_at": user["created_at"],
-        "expires_on": user.get("expires_on"),
-        "txl_config": user.get("txl_config"),
-        "ixl_config": user.get("ixl_config"),
-    }
+    user['id'] = str(user['_id'])
+    del user['_id']
+    return user
